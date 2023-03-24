64fcb1c450f903197ceb14635d028bdbbba6013e
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2023-03-24 16:46:08 +0100

    select style

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Exemple from '../components/exemple'
 import ForeCast from '../components/forecastContainer/forecastContainer'
-import SelectComponent from "../components/SelectComponent";
+import SelectComponent from '../components/select/SelectComponent'
 
 const Home = () => {
-	const [id, setId] = useState(1);
+	const [id, setId] = useState(1)
 	return (
 		<div>
-			<Exemple />
-			<SelectComponent setId={setId}/>
-			{id && (<ForeCast cityId={id}/>)}
+			{/* <Exemple /> */}
+			<h1>Weather Report</h1>
+			<SelectComponent setId={setId} />
+			{id && <ForeCast cityId={id} />}
 		</div>
 	)
 }
