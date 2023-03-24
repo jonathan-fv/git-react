3e05973e26a45b491036b1690faed222f9cbf79a
h0ldhaven <h0ldhaven@users.noreply.github.com>
2023-03-24 15:11:48 +0100

    second commit bis

---
+++ src/components/forecastContainer/forecastContainer.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react'
+import ForeCasts from '../../data.json'
+import Card from '../forcast/Card';
+import './forecastContainer.css'
+
+const forecastContainer = (props) => {
+
+    const city = "marseille" ;
+    const foreCasts = ForeCasts;
+    const cityForeCast = (cityChoosen) => foreCasts.filter((cityName) => {
+        return cityName.city.toLowerCase() === cityChoosen.toLowerCase();
+    });
+    const getDay = (currentdate) => {
+        const date = new Date(currentdate);
+        return day[date.getDay()];
+    };
+    const day = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+	return (
+		<div>
+
+            {city ? cityForeCast(city).map((foreCast, i) =>
+                <div key={i}>
+                    <h2>{foreCast.city}</h2>
+                    <div className='cards'>
+                    {foreCast.forecast_days.map((foreCastDetails, j) => {
+                        return <div key={j} className="card">
+                            <Card day={getDay(foreCastDetails.date)} currentWeather={foreCastDetails.average_weather} id={foreCastDetails.date} />
+                        </div>
+                    })}
+                    </div>
+                </div>
+            ) : null} 
+            <div>
+                
+            </div>
+		</div>
+	)
+}
+
+export default forecastContainer
